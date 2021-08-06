e3fa75900ea7dfae0c8539568008a827a5e0532b
RomaKakran <87090237+RomaKakran@users.noreply.github.com>
2021-08-06 17:47:56 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var btn_red, red_bg;
+var btn_green, green_bg;
+
+function setup() {
+createCanvas(400,400);
+
+btn_red = createButton("RED");
+btn_red.position(100,50);
+btn_green.mousePressed(red_bg);
+
+btn_green = createButton("GREEN");
+btn_green.position(250,50);
+btn_green.mousePressed(green_bg);
+}
+
+function red_bg(){
+  r = 255;
+  g = 0;
+  b = 0;
+}
+
+function green_bg(){
+  r = 0;
+  g = 255;
+  b = 0;
+}
+function draw() {
+}
